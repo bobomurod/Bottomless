60a64a073bba25909af65e787f68bb94eeedeef5
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-23 19:05:16 +0300

    postman tested succesfull

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
-console.log('hello')
+console.log('working')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -8,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world'
+    message: `Hello ${req.body.email}! your user was registred`
   })
 })
 
