90558f323dfaca1742ee2251189f03d83c353a28
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-25 02:41:33 +0300

    registration setup

--- frontend/myproject/src/components/Register.vue
+++ frontend/myproject/src/components/Register.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
 <h1>register</h1>
+<input type="email" name="email" v-model="email" placeholder="email" />
+<br>
+<input type="password" name="password" v-model="password" placeholder="password" />
+<br>
+<button @click="register" >Register me</button>
   </div>
  </template>
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
+      email: '',
+      password: ''
 
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
     }
   }
 }
