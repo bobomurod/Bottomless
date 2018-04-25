bfe6b31e4bb13ef375dfdafa6408bc1dda22887b
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-25 18:44:30 +0300

    require routes app

--- server/src/app.js
+++ server/src/app.js
@@ -4,21 +4,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const {sequelize} = require('./models')
+const config = require('./config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message1: `Hello ${req.body.email}! your user was registred`,
-    message2: `And your password is ${req.body.password}, keep it in safe pleace.`
-  })
-})
+require('./routes')(app)
 
 sequelize.sync()
   .then(() => {
-    app.listen(process.env.PORT || 8081)
+    app.listen(config.port)
     console.log(`Server started on port ${config.port}`)
   })
 
