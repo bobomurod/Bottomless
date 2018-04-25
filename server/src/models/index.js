f310e5f248d9ba0582d2ceac580427a214aef4c4
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-25 19:34:07 +0300

    modules/index.js

---
+++ server/src/models/index.js
@@ -0,0 +1,10 @@
+fs path Sequelize config
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const config = require('./config/config')
+const db = {}
+
+const sequelize = new Sequelize(
+  
+) 
