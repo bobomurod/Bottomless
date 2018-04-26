5d006c91dc7cd47a0807300f3b4222f4c76870d5
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-26 10:56:19 +0300

    nothing works

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,11 +1,13 @@
 module.export = {
-    port: 8081,
+    port: process.env.PORT || 8081,
     db: {
-      database: process.env.DB_NAME || ''
-      user:
-      password:
+      database: process.env.DB_NAME || 'dimadb',
+      user: process.env.DB_USER || 'dimadb',
+      password: process.env.DB_PASS || 'dimadb',
       options: {
-          
+        dialect: process.env.DIALECT || 'sqlite',
+        storage: './Bottomless.sqlite'
+
       }
     }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,8 @@
 module.export = (app) => {
-    app.post('/register', (req, res) => {
-        res.send({
-          message1: `Hello ${req.body.email}! your user was registred`,
-          message2: `And your password is ${req.body.password}, keep it in safe pleace.`
-        })
-      })
+  app.post('/register', (req, res) => {
+    res.send({
+      message1: `Hello ${req.body.email}! your user was registred`,
+      message2: `And your password is ${req.body.password}, keep it in safe pleace.`
+    })
+  })
 }
