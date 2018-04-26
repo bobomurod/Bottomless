4950e53b79542e911e528ec8a933aa7635a9e3de
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-26 19:54:59 +0300

    vuetifyed but not final

--- frontend/myproject/src/components/Register.vue
+++ frontend/myproject/src/components/Register.vue
@@ -1,26 +1,27 @@
 <template>
-  <div>
-
-<h1>register</h1>
-
-<input type="email" name="email" v-model="email" placeholder="email" />
-<br>
-
-<input type="password" name="password" v-model="password" placeholder="password" />
-
-<br>
-<div class="error" v-html="error" />
-<p>
-<div class="investText" v-html="investText" />
-<br>
-
-<button @click="register" >Register me</button>
-<br>
-<button @click="invest">Invest here</button>
-
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-3">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+            <div class="pl-6 pr-6 pt-2 pb-2">
+            <input type="email" name="email" v-model="email" placeholder="email" />
+            <br>
+            <input type="password" name="password" v-model="password" placeholder="password" />
+            <br>
+            <div class="error" v-html="error" />
+            <br>
+            <div class="investText" v-html="investText" />
+            <br>
+            <button @click="register">Register me</button>
+            <br>
+            <button @click="invest">Invest here</button>
+            </div>
+      </div>
+    </v-flex>
+  </v-layout>
  </template>
-
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
