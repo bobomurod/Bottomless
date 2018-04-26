9fca564855b885275e929a9f27bfccde8cac4e8e
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-26 13:14:34 +0300

    working without lint

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,12 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message1: `Hello ${req.body.email}! your user was registred`,
-      message2: `And your password is ${req.body.password}, keep it in safe pleace.`
-    })
-  })
+  app.post('/register', 
+  AuthenticationController.register)
+  // (req, res) => {
+  //   res.send({
+  //     message1: `Hello ${req.body.email}! your user was registred`,
+  //     message2: `And your password is ${req.body.password}, keep it in safe pleace.`
+  //   })
+  // })
 }
