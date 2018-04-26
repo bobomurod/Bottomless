02c5699a4f8d72afa528f2dadc3bf064657ca6b5
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-26 13:53:50 +0300

    new auth policy with email and password check

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -2,6 +2,33 @@ const Joi = require('joi')
 
 module.exports = {
     reqister (req, res, next) {
-        next()
+        const schema = {
+            email: Joi.string().email(),
+            password: Joi.string().regex(
+                new RegExp('^[a-zA-Z0-9]{2-8}$')
+            )
+        }
+
+        const {error, value} = Joi.validate)req.body, schema)
+        if (error){
+            switch (error.details[0].context.key) {
+                case 'email':
+                    res.status(400).send({
+                        error: 'You must provide valid email'
+                    })
+                    break
+                case 'password':
+                    res.status(400).send({
+                        error: 'Password must be difficult'
+                    })
+                    break
+                default:
+                    res.status(400).send({
+                        error: 'Invalid registration information'
+                    })
+            }
+        }else{
+            next()
+        }
     }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,11 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 
+const AuthenticationControllerPolicy = require('./policies/AuthenticationcontrollerPolicy')
+
+
 module.exports = (app) => {
   app.post('/register', 
+  AuthenticationControllerPolicy.register,
   AuthenticationController.register)
   // (req, res) => {
   //   res.send({
