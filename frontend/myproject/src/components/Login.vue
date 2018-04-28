ab480ccfdb15eaa5c2a9c6a0b4b0044aa47299c8
bobomurod <bobomurod.boboev@yandex.ru>
2018-04-28 18:21:55 +0300

    modefying login vue

--- frontend/myproject/src/components/Login.vue
+++ frontend/myproject/src/components/Login.vue
@@ -36,9 +36,9 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        const response = await AuthenticationService.register({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
