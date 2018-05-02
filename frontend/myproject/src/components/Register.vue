7d0e00313e009e57888b6775ffa2abf29be14d25
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-03 00:05:35 +0300

    dispatch register

--- frontend/myproject/src/components/Register.vue
+++ frontend/myproject/src/components/Register.vue
@@ -40,6 +40,8 @@ export default {
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
         // this.st = 'good'
       } catch (error) {
         this.error = error.response.data.error
