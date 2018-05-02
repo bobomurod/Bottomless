7134402b9eb5c64b981ba9752c7da45b435d5030
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-02 23:15:34 +0300

    full working with JWT and bcrypt

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -24,8 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING
   }, {
     hooks: {
-      beforeCreate: hashPassword,
-      beforeUpdate: hashPassword,
+      
       beforeSave: hashPassword
   }
   })
