4a7d5c28d858499c68acc511c8759331d1c45b21
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-17 13:24:25 +0300

    erc20 basic

--- contracts/token.sol
+++ contracts/token.sol
@@ -36,3 +36,18 @@ library SafeMath {
     return c;
   }
 }
+
+////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @title ERC20Basic
+ * @dev Самая простая версия ERC20 интерфейса
+ * @dev подробно смотрите на https://github.com/ethereum/EIPs/issues/179
+ */
+
+contract ERC20Basic {
+    uint256 public totalSupply;  // totalSupply - общая эмиссия
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
